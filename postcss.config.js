88c21e5e6b331e0b5c189aab288a9de493db8ad6
John L. Armstrong IV <jlarmst@g.clemson.edu>
2020-04-06 10:37:18 -0400

    fix: postcss with purgecss and psuedo classes

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,10 @@ module.exports = {
     // https://tailwindcss.com/docs/using-with-preprocessors#future-css-features
     require('postcss-preset-env')({
       stage: 1,
+      features: {
+        // https://github.com/tailwindcss/tailwindcss/issues/1190#issuecomment-546621554
+        'focus-within-pseudo-class': false,
+      },
       autoprefixer: process.env.NODE_ENV === 'production',
     }),
 
@@ -13,7 +17,7 @@ module.exports = {
       ? require('cssnano')({ preset: 'default' })
       : null,
     process.env.NODE_ENV === 'production'
-      ? require('purgecss')({
+      ? require('@fullhuman/postcss-purgecss')({
           // Specify the paths to all of the template files in your project
           content: [
             './src/**/*.js',
